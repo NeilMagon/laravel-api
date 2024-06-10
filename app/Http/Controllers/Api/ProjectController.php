a965fa43bcf8609ac88dadec5f1c86e701977a41
NeilMagon <neil.magon97@gmail.com>
2024-06-10 18:01:48 +0200

    feat: add api controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    public function index(){
+        $project = Project::all();
+        
+        return response()->json([
+            'success' => true,
+            'results' => $project
+        ]);
+    }
 }
